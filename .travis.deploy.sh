6178bdb3d295635e68f3702c9ad58f58d67da83a
xonix <xonixx@gmail.com>
2017-10-26 17:19:51 +0300

    deploy

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -2,7 +2,7 @@
 
 set -v
 
-TARGET=www.icosurance.com
+TARGET=airouting.cmlteam.com
 
 ssh -i /tmp/deploy_rsa apps1@prod.cmlteam.com "(echo \"\$(date) $TARGET \$(pwd)\" ; rm -rf $TARGET ; mkdir $TARGET) >> /tmp/.deploy 2>&1"
 cd _site ; scp -i /tmp/deploy_rsa -C -r * apps1@prod.cmlteam.com:~/$TARGET
